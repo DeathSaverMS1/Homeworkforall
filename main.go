c00616e9147497bd8d34b8970d1baeabd8463cbf
DeathSaverMS1 <manvikg23@gmail.com>
2024-10-09 17:41:04 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://www.google.com/", nil
+            return "http://codesandbox.io/p/github/sauc23/v4", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
